43007948f4bc6082f754924c36b06bd45283d1da
Mark <mjfaga@users.noreply.github.com>
2025-08-26 18:30:31 -0400

    fix: add responseHeaders to typings.d.ts
    Updates the typings to include the {responseHeaders} object in the `onNext` callback.

--- typings.d.ts
+++ typings.d.ts
@@ -12,7 +12,7 @@ declare function MultipartFetchFunction<T = unknown>(url: string, params: {
     headers?: Record<string, string>;
     credentials?: string;
     body?: string;
-    onNext: (result: T[]) => void;
+    onNext: (result: T[], {responseHeaders}: {responseHeaders: string | string[]}) => void;
     onError: (error: unknown) => void;
     onComplete : () => void
 }): void
